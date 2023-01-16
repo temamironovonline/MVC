ba340458dfcfea476bf3fc605361c419936618e6
Artem Sergeichev <a.sergeichev03@mail.ru>
2023-01-16 14:43:33 +0300

    Добавьте файлы проекта.

---
+++ MVC1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace MVC1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            actionComboBox.ItemsSource = Model.calculateManipulation;
+            Model.firstNumber = firstNumber;
+            Model.secondNumber = secondNumber;
+            Model.answer = resultTextBox;
+            Model.calculateSymbol = operationSymbol;
+            actionComboBox.SelectedIndex = 0;
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Model.GetAnswer = actionComboBox.SelectedIndex;
+        }
+
+        private void actionComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Model.GetSymbolCalculating = actionComboBox.SelectedIndex;
+        }
+
+        private void firstNumber_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            int val;
+            if (!Int32.TryParse(e.Text, out val) && e.Text != ",")
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ MVC1/Model.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVC1
+{
+    internal class Model
+    {
+        public static List<string> calculateManipulation = new List<string> { "Сложение", "Вычитание", "Умножение", "Деление" };
+        public static List<string> operationSymbols = new List<string> { "+", "-", "*", "/" };
+
+
+        public static TextBox firstNumber;
+        public static TextBox secondNumber;
+        public static TextBox answer;
+        public static TextBlock calculateSymbol;
+
+        public static double GetAnswer
+        {
+            get
+            {
+                return GetAnswer;
+            }
+            set
+            {
+                try
+                {
+                    switch (value)
+                    {
+                        case 0:
+                            answer.Text = Convert.ToString(Convert.ToDouble(firstNumber.Text) + Convert.ToDouble(secondNumber.Text));
+                            break;
+                        case 1:
+                            answer.Text = Convert.ToString(Convert.ToDouble(firstNumber.Text) - Convert.ToDouble(secondNumber.Text));
+                            break;
+                        case 2:
+                            answer.Text = Convert.ToString(Convert.ToDouble(firstNumber.Text) * Convert.ToDouble(secondNumber.Text));
+                            break;
+                        case 3:
+                            answer.Text = Convert.ToString(Convert.ToDouble(firstNumber.Text) / Convert.ToDouble(secondNumber.Text));
+                            break;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка!");
+                }
+            }
+        }
+
+        public static int GetSymbolCalculating
+        {
+            set
+            {
+                calculateSymbol.Text = operationSymbols[value];
+            }
+        }
+
+    }
+}
